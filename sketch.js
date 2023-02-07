62c1a55af004313dde1df448e42b5b78faa43f77
Anton Plesca <anton.plesca@yahoo.com>
2023-02-06 19:36:41 -0700

    tamagotchi size change

--- sketch.js
+++ sketch.js
@@ -9,11 +9,11 @@ function setup()
     
     tamagotchi = {
         points: [],
-        size: 200,
+        size: 100,
         setup: function(){
             var incr = PI * 2/36 ;
             for(var i = 0; i < 36; i++){
-                var v = createVector(0, 1);
+                var v = createVector(0, random(0.75, 1));
                 var a = incr * i;
                 v.rotate(a);
                 this.points.push(v);
@@ -24,12 +24,19 @@ function setup()
             beginShape();
             for(var i = 0; i < this.points.length; i++){
                 var v = p5.Vector.mult(this.points[i], this.size);
-                vertex(
+                curveVertex(
                     v.x,
                     v.y
                 )
             }
             endShape();
+        },
+        grow: function(){
+            this.size *= random(1, 1.005);
+        },
+        shrink: function(){
+            this.size *=random(1, 0.9995);
+            this.size = max(30, this.size);
         }
     }
 
@@ -44,13 +51,14 @@ function draw()
     background(0,0,0);
     translate(width/2,height/2);
     
-    tamagotchi.draw()
+    tamagotchi.draw();
+    tamagotchi.shrink();
 
 }
 
 function mouseDragged()
 {
-  
+  tamagotchi.grow()
 
 }
 
